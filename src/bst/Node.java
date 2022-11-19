92b1f41c9d0d55e2518f538d12129af6a38a0810
PotatoManFelix <mst307@student.bau.edu.lb>
2022-11-19 02:51:18 +0200

    added node and tree

---
+++ src/bst/Node.java
@@ -0,0 +1,12 @@
+package bst;
+public class Node {
+    int data;
+    String name;
+    Node left;
+    Node right;
+    public Node(int score,String name){
+        data=score;
+        this.name=name;
+    }
+}
+
